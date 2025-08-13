2c0262dfd6fa1b678dd1085b735ab4a4809a84a2
shaiksanda <shaiksandanibasha7@gmail.com>
2025-08-13 16:07:13 +0530

    Added unique id and broadcast msgs to all clients

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+const { v4: uuidv4 } = require("uuid");
 const WebSocket = require('ws'); // load the 'ws' library
 // create a WebSocket server that listens on port 8080
 const wss = new WebSocket.Server({ port: 6002 }, () => {
     console.log('WebSocket server listening on ws://localhost:6002');
 });
-wss.on("connection", (ws, req) => {
-    console.log("New Client Connected")
-    ws.send("Welcome From Websocket Server!")
-    ws.on("message",(msg)=>{
-        console.log(`Message From Client: ${msg}`)
-        ws.send('Hello from server! I got your message.');
-    })
-    ws.on("close",()=>{
-        console.log("Client Is Disconnected")
-    })
-    ws.on("error",(err)=>{
-        console.log(`Websocket Error: ${err}`)
-    })
+
+const broadcast = (senderId, message) => {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN && client.id !== senderId) {
+            client.send(`Client ${senderId}: ${message}`);
+        }
+    });
+};
+
+
+wss.on("connection", (ws) => {
+    ws.id = uuidv4()
+    console.log(`New Client Connected (ID: ${ws.id})  Total clients: ${wss.clients.size}`);
+
+    ws.send(`Welcome! You are Client ${ws.id}`);
+
+    ws.on("message", (msg) => {
+        console.log(`Message From Client ${ws.id}: ${msg}`);
+        
+        ws.send(`Server Received: ${msg}`);
+
+        broadcast(ws.id, msg)
+    });
+
+    ws.on("close", () => {
+        console.log(`A Client ${ws.id} Disconnected,  Total clients: ${wss.clients.size}`);
+        
+    });
+
+    ws.on("error", (err) => {
+        console.log(`WebSocket Error from Client ${ws.id}: ${err}`);
+    });
 })
